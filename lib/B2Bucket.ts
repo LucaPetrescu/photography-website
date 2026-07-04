6a3501c33d076388931333856674bda8af80c977
LucaPetrescu <luca6011@gmail.com>
2026-07-05 00:02:32 +0300

    feat: region

--- lib/B2Bucket.ts
+++ lib/B2Bucket.ts
@@ -28,7 +28,7 @@ function getS3(): {
   const region = process.env.REGION;
   const bucket = process.env.BUCKET;
 
-  console.log(region);
+  console.log("Region: ", region);
 
   if (!region) throw new Error("Missing env var: REGION");
   if (!bucket) throw new Error("Missing env var: BUCKET");
